a76091796a0786fd06f1ba37d52504d00abbba01
Kaleb <kaleb21-19@github.com>
2026-05-06 16:05:17 +0800

    feat: add Hive local database setup and local storage service

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -3,8 +3,12 @@
 import 'package:connectivity_plus/connectivity_plus.dart' show Connectivity;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:newsflow/core/network/api_client.dart';
 import 'package:newsflow/core/network/network_info.dart';
+import 'package:newsflow/core/storage/hive_boxes.dart';
+import 'package:newsflow/core/storage/local_storage.dart';
 import 'package:newsflow/core/storage/secure_storage.dart';
 
 /// Global dependency injection container
@@ -14,8 +18,24 @@ final getIt = GetIt.instance;
 /// Set up all dependencies
 Future<void> setupDependencies() async {
   // Initialize all dependencies here
+    await _initHive();
    await _registerCore();
 }
+//
+Future<void> _initHive() async {
+  // Initialize Hive with Flutter path
+  await Hive.initFlutter();
+
+  // Register TypeAdapters here later
+  // Hive.registerAdapter(ArticleModelAdapter());
+
+  // Open all boxes at startup
+  await Hive.openBox(HiveBoxes.articles);
+  await Hive.openBox(HiveBoxes.savedArticles);
+  await Hive.openBox(HiveBoxes.user);
+;
+}
+
 
 /// Register core dependencies
 Future<void> _registerCore() async {
@@ -23,6 +43,8 @@ Future<void> _registerCore() async {
   const androidOptions = AndroidOptions(encryptedSharedPreferences: true);
 
    getIt.registerLazySingleton<SecureStorage>(() => SecureStorage(FlutterSecureStorage(aOptions: androidOptions)));   
+
+   getIt.registerLazySingleton<LocalStorage>(() => LocalStorage());
 
 //network
   getIt.registerLazySingleton<Connectivity>(
---
+++ lib/core/storage/hive_boxes.dart
@@ -0,0 +1,7 @@
+class HiveBoxes {
+  HiveBoxes._();
+
+  static const String articles = 'articles_box';
+  static const String savedArticles = 'saved_articles_box';
+  static const String user = 'user_box';
+}
