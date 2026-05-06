4b1a7649cf1d9e8e2d9ac29f4798444fef935ab6
Kaleb <kaleb21-19@github.com>
2026-05-06 19:53:18 +0800

    feat:add user model with freezed v3

---
+++ lib/features/auth/data/models/user_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_UserModel _$UserModelFromJson(Map<String, dynamic> json) => _UserModel(
+  id: json['id'] as String,
+  email: json['email'] as String,
+  firstName: json['first_name'] as String,
+  lastName: json['last_name'] as String,
+  avatar: json['avatar'] as String,
+  token: json['token'] as String,
+);
+
+Map<String, dynamic> _$UserModelToJson(_UserModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'first_name': instance.firstName,
+      'last_name': instance.lastName,
+      'avatar': instance.avatar,
+      'token': instance.token,
+    };
